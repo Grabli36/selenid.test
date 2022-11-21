2da7d0fa10ade40851673599342be4343b7a8e23
Евгений Ухаткин <ukhatkin99@mail.ru>
2022-11-21 10:51:22 +0700

    Сделал 3 метода Подключение к android

--- src/test/java/TestSelenoid.java
+++ src/test/java/TestSelenoid.java
@@ -1,42 +1,44 @@
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$x;
-import static com.codeborne.selenide.Selenide.open;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class TestSelenoid {
+    static AndroidDriver driver;
 
-    @Test
-    @DisplayName("Тест авторизации")
-    public void testAuthorization() throws InterruptedException {
+        @BeforeTest
+        public void setUp() throws MalformedURLException {
 
-        String buttonPhoneAuthorization = "//button[contains(text(),'По номеру телефона')]";
-        String buttonProfile = "//*[@id=\"btn_profile\"]";
-        String inputPhoneNumber = " //input[@class='sc-cSHVUG GWKti']";
-        String buttonSend = "//button[@class='sc-kpOJdX rkQYm']";
-        String inputCod = "//input[@class='sc-cSHVUG cYBPsl']";
-        String buttonAuthorization = "//button[contains(text(),'Авторизоваться')]";
-        String iconVk = "//a[@href='/social-auth/vkontakte']";
-        String iconGmail = "//a[@href='/social-auth/vkontakte']";
-        String iconFaceBook = "//a[@href='/social-auth/facebook']";
-        String Goodsession = "//button[contains(text(),'Вернуться назад')]";
+            DesiredCapabilities cap = new DesiredCapabilities();
 
+            cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
+            cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9");
+            cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UITest");
+            cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Google Pixel 3");
+            cap.setCapability(MobileCapabilityType.APP, "Users/ukhatkin.ea/Desktop/webdraiver/communals.apk/");
 
-        open("https://saint-petersburg.kaverafisha.ru");
-        $x(buttonProfile).shouldBe(visible).click();
-        $x(iconVk).shouldHave(attribute("class", "sc-kEYyzF cHBpjg"));
-        $x(iconGmail).shouldHave(attribute("class", "sc-kEYyzF cHBpjg"));
-        $x(iconFaceBook).shouldHave(attribute("class", "sc-kEYyzF cHBpjg"));
-        $x(buttonPhoneAuthorization).shouldBe(visible).click();
-        $x(inputPhoneNumber).setValue("9999999999");
-        $x(buttonSend).click();
-        $x(inputCod).setValue("3376").shouldBe(visible);
-        $x(buttonAuthorization).click();
-        Thread.sleep(13000);
+            driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
 
-    }
+        }
+
+        @Test
+        public  void test(){
+            /*MobileElement el1 = (MobileElement) driver.findElementById();*/
 
 
+
+        }
+
+        @AfterTest
+        public void end(){
+            driver.quit();
+        }
 }
