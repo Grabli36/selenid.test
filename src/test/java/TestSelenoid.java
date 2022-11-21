01e693813c343067770b09096e522b2430dd9145
Евгений Ухаткин <ukhatkin99@mail.ru>
2022-11-21 14:14:11 +0700

    Добавил junit

--- src/test/java/TestSelenoid.java
+++ src/test/java/TestSelenoid.java
@@ -1,7 +1,6 @@
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
@@ -13,32 +12,37 @@ import java.net.URL;
 
 public class TestSelenoid {
     static AndroidDriver driver;
+    String PAGE_ACTIVITY = ".presentation.launch.LaunchActivity";
+    String APP_PACKAGE = "info.goodline.communals";
 
-        @BeforeTest
-        public void setUp() throws MalformedURLException {
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
 
-            DesiredCapabilities cap = new DesiredCapabilities();
-
-            cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
-            cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9");
-            cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UITest");
-            cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Google Pixel 3");
-            cap.setCapability(MobileCapabilityType.APP, "Users/ukhatkin.ea/Desktop/webdraiver/communals.apk/");
-
-            driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
-
-        }
-
-        @Test
-        public  void test(){
-            /*MobileElement el1 = (MobileElement) driver.findElementById();*/
+        DesiredCapabilities cap = new DesiredCapabilities();
 
 
+        cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9");
+        cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UITest");
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Google Pixel 3");
+        cap.setCapability(MobileCapabilityType.APP, "Users/ukhatkin.ea/Desktop/webdraiver/communals.apk/");
+        cap.setCapability("appPackage",APP_PACKAGE);
+        cap.setCapability("appActivity",PAGE_ACTIVITY);
 
-        }
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
 
-        @AfterTest
-        public void end(){
-            driver.quit();
-        }
+    }
+
+    @Test
+    public void test() {
+        MobileElement el1 = (MobileElement) driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Phone\"]");
+        el1.click();
+
+
+    }
+
+    @AfterTest
+    public void end() {
+        driver.quit();
+    }
 }
