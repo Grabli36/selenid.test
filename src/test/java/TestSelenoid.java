ddd391b961b5d6b5a0b7e68e55e7d63a21121a76
Евгений Ухаткин <ukhatkin99@mail.ru>
2022-11-22 15:37:36 +0700

    Написал тест, открытия приложение

--- src/test/java/TestSelenoid.java
+++ src/test/java/TestSelenoid.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.codeborne.selenide.WebDriverRunner;
 
+import java.util.concurrent.TimeUnit;
+
 public class TestSelenoid {
     static AndroidDriver driver;
     static DesiredCapabilities capabilities;
@@ -16,10 +18,10 @@ public class TestSelenoid {
         capabilities.setCapability("appActivity", ".presentation.launch.LaunchActivity");
         capabilities.setCapability("appPackage", "info.goodline.communals");
         capabilities.setCapability(MobileCapabilityType.APP,
-                "Users/ukhatkin.ea/Desktop/webdraiver/communals.apk/");
+                "C:\\Users/ukhatkin.ea/Desktop/webdraiver/communals.apk");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Google Pixel 3");
-        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UITest");
+/*        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UITest");*/
         capabilities.setCapability("uiautomator2ServerInstallTimeout", "90000");
         capabilities.setCapability("skipUnlock", true);
         capabilities.setCapability("uninstallOtherPackages", "true");
@@ -36,9 +38,12 @@ public class TestSelenoid {
     }
 
     @Test
-    public void test() {
-        MobileElement el1 = (MobileElement) driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Phone\"]");
-        el1.click();
+    public void test() throws InterruptedException {
+        MobileElement el1 = (MobileElement) driver.findElementById("info.goodline.communals:id/flBtnNext");
+        for (int i = 0; i < 4; i++) {
+            el1.click();
+        }
+        TimeUnit.SECONDS.sleep(3);
     }
 
     @AfterEach
